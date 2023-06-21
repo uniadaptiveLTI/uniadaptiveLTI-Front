fc65251b40440f3984c8edcfcf791087aba884ba
David Jiménez <davidjimenezdaroqui@gmail.com>
2023-06-21 13:20:19 +0200

    Revert "Peticiones back"
    This reverts commit 0575300d32cc5b821e9d1265ab76f744e95ffb12.

--- pages/api/getResourceUsage.js
+++
@@ -1,9 +0,0 @@
-export default function handler(req, res) {
-	try {
-		res.status(200).json({
-			...process.resourceUsage(),
-		});
-	} catch (e) {
-		res.status(500).json({ error: "failed to load server information" });
-	}
-}
