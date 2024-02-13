34d6f47704dd8901dc14b14aaf66704d00c50aa9
José Javier Sánchez Piedras <eljaviondo@gmail.com>
2024-02-13 13:23:54 +0100

    revert getVersion changes

--- components/Header.tsx
+++ components/Header.tsx
@@ -223,9 +223,7 @@ function Header({ LTISettings }, ref) {
 							const VERSIONS_RESPONSE = await getVersions(MAP_DATA.created_id);
 							if (VERSIONS_RESPONSE.ok) {
 								const VERSIONS_DATA = VERSIONS_RESPONSE.data;
-								const VERSION_RESPONSE = await getVersion({
-									version_id: VERSIONS_DATA[0].id,
-								});
+								const VERSION_RESPONSE = await getVersion(VERSIONS_DATA[0].id);
 								setVersions(VERSIONS_DATA);
 								if (VERSION_RESPONSE.ok) {
 									setSelectedVersion(VERSION_RESPONSE.data as IVersion);
@@ -522,9 +520,9 @@ function Header({ LTISettings }, ref) {
 				} else {
 					setVersions(VERSIONS_RESPONSE.data);
 
-					const VERSION_RESPONSE = await getVersion({
-						version_id: VERSIONS_RESPONSE.data[0].id,
-					});
+					const VERSION_RESPONSE = await getVersion(
+						VERSIONS_RESPONSE.data[0].id
+					);
 
 					if (!VERSION_RESPONSE.ok) {
 						toast(
@@ -1113,7 +1111,7 @@ function Header({ LTISettings }, ref) {
 	async function fetchVersion(versionId) {
 		if (!parseBool(process.env.NEXT_PUBLIC_DEV_FILES)) {
 			try {
-				const VERSION_RESPONSE = await getVersion({ created_id: versionId });
+				const VERSION_RESPONSE = await getVersion(versionId);
 				if (VERSION_RESPONSE.ok) {
 					setCurrentBlocksData(VERSION_RESPONSE.data.blocks_data);
 				} else {
--- middleware/api/getVersion.tsx
+++ middleware/api/getVersion.tsx
@@ -11,23 +11,16 @@ interface GetVersionResponse extends ICommonValidResponse {
 }
 
 interface Payload {
-	version_id?: number;
-	created_id?: number;
+	version_id: number;
 }
 
 /**
  *
- * @param version_id DB's id
- * @param created_id Front end id (Defaults to this one)
+ * @param versionId DB's created_id
  * @returns
  */
-export default async function getVersion({ version_id, created_id }: Payload) {
-	if (created_id != undefined) {
-		return fetchBackEnd(getLocalToken(), "api/lti/get_version", "POST", {
-			created_id: created_id,
-		}) as Promise<GetVersionResponse> | Promise<ICommonInvalidResponse>;
-	}
+export default async function getVersion(versionId: number) {
 	return fetchBackEnd(getLocalToken(), "api/lti/get_version", "POST", {
-		version_id: version_id,
+		version_id: versionId,
 	}) as Promise<GetVersionResponse> | Promise<ICommonInvalidResponse>;
 }
